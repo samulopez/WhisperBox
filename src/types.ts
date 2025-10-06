ee9d4dfe76a607000c6255db1c2ed394e5a0e9f0
Samuel Lopez <samulopez@gmail.com>
2025-10-06 16:53:09 +0200

    feat: allow drag and drop

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
 export interface WhisperBoxApi {
   createWhisperBox: (data: { name: string; targetUser: string }) => void;
 }
+
+export interface WhisperBoxChatMessage {
+  speaker: string | undefined;
+  whisperedTo: string | undefined;
+  content: string;
+  className: string;
+}
